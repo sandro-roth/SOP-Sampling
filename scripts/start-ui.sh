918dca1fbb1ac47df4b6b7c22c028efbfe2e7104
sandro-roth <sandro.roth@hotmail.de>
2025-11-10 16:51:39 +0100

    create start script for UI

--- scripts/start-ui.sh
+++ scripts/start-ui.sh
@@ -6,3 +6,7 @@ set -euo pipefail
 
 # Start UI application
 echo 'Starting UI service ...'
+# src importable
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export PYTHONPATH="$SCRIPT_DIR/../src"
+exec python -m sop_ui.app
