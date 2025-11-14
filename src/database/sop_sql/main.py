08a6717c4e95b1c6d70b136e14585e3537ad0edc
sandro-roth <sandro.roth@hotmail.de>
2025-11-15 00:26:24 +0100

    fix import + load in with contextmanager

--- src/database/sop_sql/main.py
+++ src/database/sop_sql/main.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
 
-from utils import setup_logging, get_logger, __load_env
-from function_table import CREATE_FUNCTION_TABLE
+from utils import setup_logging, get_logger, __load_env, db_conn
+from .function_table import CREATE_FUNCTION_TABLE
 
 def main():
     cwd = Path(__file__).resolve()
@@ -11,7 +11,9 @@ def main():
     db_log = get_logger(__name__)
     db_log.info(f".env loaded from: {loaded_from}")
     db_log.info('---- Database script running ----')
-    # check for tables
+
+    with db_conn(os.getenv('DATA_DIR')) as (con, cur):
+        cur.execute(CREATE_FUNCTION_TABLE)
     # if no tables create them
 
 
