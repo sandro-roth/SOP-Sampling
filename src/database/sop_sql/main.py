227885ac6232a09a2e7d1b82a50221b113304b43
sandro-roth <sandro.roth@hotmail.de>
2025-11-14 17:09:40 +0100

    + __load_env + logging

--- src/database/sop_sql/main.py
+++ src/database/sop_sql/main.py
@@ -1,7 +1,23 @@
+import os
+from pathlib import Path
+
+from utils import setup_logging, get_logger, __load_env
+
+# Setup
+cwd = Path(__file__).resolve()
+loaded_from = __load_env(cwd=cwd)
+
+
 def main():
-    print('update your code first')
+    setup_logging(app_name='database', log_dir=os.getenv('DB_LOG_DIR'), to_stdout=False)
+    db_log = get_logger(__name__)
+    db_log.info('---- Database script running ----')
     # check for tables
     # if no tables create them
 
 
 # Interact class (loading a table then write methods to pull, push/add, delete)
+
+
+if __name__ == '__main__':
+    main()
