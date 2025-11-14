9dbe36bac7acf46d6b957150f216598ff38e1b3b
sandro-roth <sandro.roth@hotmail.de>
2025-11-14 23:40:07 +0100

    minor changes

--- src/database/sop_sql/main.py
+++ src/database/sop_sql/main.py
@@ -3,14 +3,12 @@ from pathlib import Path
 
 from utils import setup_logging, get_logger, __load_env
 
-# Setup
-cwd = Path(__file__).resolve()
-loaded_from = __load_env(cwd=cwd)
-
-
 def main():
+    cwd = Path(__file__).resolve()
+    loaded_from = __load_env(cwd=cwd)
     setup_logging(app_name='database', log_dir=os.getenv('DB_LOG_DIR'), to_stdout=False)
     db_log = get_logger(__name__)
+    db_log.info(f".env loaded from: {loaded_from}")
     db_log.info('---- Database script running ----')
     # check for tables
     # if no tables create them
