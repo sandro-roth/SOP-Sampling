ad0a9db733bc2ebe6a9b3c7ccaf8a42bcb3e35b6
sandro-roth <sandro.roth@hotmail.de>
2025-11-15 00:34:36 +0100

    Create tables annotatoins and users

--- src/database/sop_sql/main.py
+++ src/database/sop_sql/main.py
@@ -3,10 +3,13 @@ from pathlib import Path
 
 from utils import setup_logging, get_logger, __load_env, db_conn
 from .function_table import CREATE_FUNCTION_TABLE
+from .user_table import CREATE_USER_TABLE
+from .annotations_table import CREATE_ANNOTATION_TABLE
 
 def main():
     cwd = Path(__file__).resolve()
     loaded_from = __load_env(cwd=cwd)
+
     setup_logging(app_name='database', log_dir=os.getenv('DB_LOG_DIR'), to_stdout=False)
     db_log = get_logger(__name__)
     db_log.info(f".env loaded from: {loaded_from}")
@@ -14,7 +17,9 @@ def main():
 
     with db_conn(os.getenv('DATA_DIR')) as (con, cur):
         cur.execute(CREATE_FUNCTION_TABLE)
-    # if no tables create them
+        cur.execute(CREATE_USER_TABLE)
+        cur.execute(CREATE_ANNOTATION_TABLE)
+
 
 
 # Interact class (loading a table then write methods to pull, push/add, delete)
