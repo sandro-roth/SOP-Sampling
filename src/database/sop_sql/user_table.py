5c8e4db27f112c65df5379a2eba1b4f8b9b49cb0
sandro-roth <sandro.roth@hotmail.de>
2025-11-15 00:41:05 +0100

    adapted to actual user_table

--- src/database/sop_sql/user_table.py
+++ src/database/sop_sql/user_table.py
@@ -1,7 +1,10 @@
 # This is the Schema to create the function_table
 CREATE_USER_TABLE = """
-CREATE TABLE IF NOT EXISTS function (
+CREATE TABLE IF NOT EXISTS user (
     Id INTEGER PRIMARY KEY AUTOINCREMENT,
-    function_name TEXT NOT NULL
+    Name TEXT NOT NULL,
+    function INTEGER NOT NULL,
+    years_in_the_function INTEGER,
+    FOREIGN KEY (function) REFERENCES function(Id)
 );
 """
