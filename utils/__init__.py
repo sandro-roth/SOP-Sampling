75fe3e3237e69b2fb946768ce30eba95b550921f
sandro-roth <sandro.roth@hotmail.de>
2025-11-15 01:13:40 +0100

    + preview_db function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .logger import setup_logging, get_logger
-from .database import db_conn, db_push, db_pull
+from .database import db_conn, db_push, db_pull, preview_db
 from .load_env import __load_env
