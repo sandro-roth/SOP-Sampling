f737a7ad5cb2cf0b91fea9372890d97fd8e512d8
sandro-roth <sandro.roth@hotmail.de>
2025-11-17 15:46:46 +0100

    Table for questions and answers

---
+++ src/database/sop_sql/question_bank.py
@@ -0,0 +1,9 @@
+# This is the Schema to create the question_table
+CREATE_QUESTIONS_TABLE = """
+CREATE TABLE IF NOT EXISTS questions (
+    question_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    passage TEXT NOT NULL
+);
+"""
