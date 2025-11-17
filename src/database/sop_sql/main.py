458dd013c2b76ec6af6f7412ce7c21f94fd49c1f
sandro-roth <sandro.roth@hotmail.de>
2025-11-17 20:36:45 +0100

    testing push to questions table

--- src/database/sop_sql/main.py
+++ src/database/sop_sql/main.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-from utils import setup_logging, get_logger, __load_env, db_conn, preview_db
+from utils import setup_logging, get_logger, __load_env, db_conn, preview_db, db_push, load_yaml
 from .function_table import CREATE_FUNCTION_TABLE
 from .user_table import CREATE_USER_TABLE
 from .annotations_table import CREATE_ANNOTATION_TABLE
@@ -10,6 +10,7 @@ from .question_bank import CREATE_QUESTIONS_TABLE, CREATE_BACKUP
 def main():
     cwd = Path(__file__).resolve()
     loaded_from = __load_env(cwd=cwd)
+    statements = load_yaml()
 
     setup_logging(app_name='database', log_dir=os.getenv('DB_LOG_DIR'), to_stdout=False)
     db_log = get_logger(__name__)
@@ -27,6 +28,15 @@ def main():
     with db_conn(qdb_path) as (con, cur):
         cur.execute(CREATE_QUESTIONS_TABLE)
         cur.execute(CREATE_BACKUP)
+
+    # check if pushing possible
+    question_data = [
+        ('is it possible', 'yes it is', 'the passage this is from'),
+        ('does it look different', 'no it does not', 'no passage'),
+        ('is this the third time?', 'maybe it is ', 'passage one'),
+        ('....?', '....!', '....')
+    ]
+    db_push(question_data, qdb_path, 'questions', statements=statements)
 
     preview_db(db_path)
     preview_db(qdb_path)
