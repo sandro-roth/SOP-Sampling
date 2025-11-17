5583786e62bc8e75a17c4409e404524c555af645
sandro-roth <sandro.roth@hotmail.de>
2025-11-17 15:47:42 +0100

    create questions_db + questions_answers table

--- src/database/sop_sql/main.py
+++ src/database/sop_sql/main.py
@@ -5,6 +5,7 @@ from utils import setup_logging, get_logger, __load_env, db_conn, preview_db
 from .function_table import CREATE_FUNCTION_TABLE
 from .user_table import CREATE_USER_TABLE
 from .annotations_table import CREATE_ANNOTATION_TABLE
+from .question_bank import CREATE_QUESTIONS_TABLE
 
 def main():
     cwd = Path(__file__).resolve()
@@ -16,13 +17,18 @@ def main():
     db_log.info('---- Database script running ----')
 
     db_path = os.getenv('DATA_DIR')
+    qdb_path = os.getenv('DATA_DIR_QUESTIONS')
 
     with db_conn(db_path) as (con, cur):
         cur.execute(CREATE_FUNCTION_TABLE)
         cur.execute(CREATE_USER_TABLE)
         cur.execute(CREATE_ANNOTATION_TABLE)
 
+    with db_conn(qdb_path) as (con, cur):
+        cur.execute(CREATE_QUESTIONS_TABLE)
+
     preview_db(db_path, pre_dir=os.getenv('PREVIEW_DIR'))
+    preview_db(qdb_path, pre_dir=os.getenv('PREVIEW_DIR'))
 
 
 # Interact class (loading a table then write methods to pull, push/add, delete)
