944730908abba1f27d2233ac4ea6fdf1ffcde30d
sandro-roth <sandro.roth@hotmail.de>
2025-11-24 18:06:26 +0100

    init Flask setup

--- src/identify/user_mask/app.py
+++ src/identify/user_mask/app.py
@@ -1,2 +1,67 @@
-def main():
-    print('test')
+import os
+
+from flask import Flask, render_template, request
+from pathlib import Path
+
+from utils import setup_logging, get_logger, __load_env
+
+
+
+# Setup
+cwd = Path(__file__).resolve()
+loaded_from = __load_env(cwd=cwd)
+
+# Example function choices for the dropdown
+FUNCTION_CHOICES = [
+    "Physician",
+    "Nurse",
+    "Researcher",
+    "Student",
+    "Technician",
+    "Other"
+]
+
+def create_app() -> Flask:
+    """
+    Create and configure the Flask application.
+
+    This function initializes a Flask app instance, sets up basic configuration,
+    and defines routes for adding parameter Values to Database
+
+    Routes:
+        GET /                       –
+        POST /submit_annotation     –
+
+    Returns:
+        Flask: The configured Flask application instance.
+    """
+
+    app = Flask(__name__,
+                template_folder=str(cwd.parent / 'templates'),
+                static_folder=str(cwd.parent / 'static'))
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    flask_log = get_logger(__name__)
+
+
+    @app.route('/', methods=['GET', 'POST'])
+    def home():
+        # Load one question
+        flask_log.info('New Question loaded')
+
+
+
+
+
+def main() -> None:
+    """
+
+    """
+
+    setup_logging(app_name='User_Mask', log_dir=os.getenv('UUI_LOG_DIR'))
+    log = get_logger(__name__)
+
+    app = create_app()
+    port = int(os.getenv("SOP_UUI_PORT", "8100"))
+    log.info(f".env loaded from: {loaded_from}")
+    log.info(f"SOP_UUI_PORT = {port}")
+    app.run(host='0.0.0.0', port=port, debug=False)
