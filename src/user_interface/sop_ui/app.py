f3a4ee7d5dc1119991872b2141c5569c787fae6e
sandro-roth <sandro.roth@hotmail.de>
2025-12-05 12:35:22 +0100

    fixed function: save_annotation_to_db

--- src/user_interface/sop_ui/app.py
+++ src/user_interface/sop_ui/app.py
@@ -12,6 +12,19 @@ loaded_from = __load_env(cwd=cwd)
 log_loc = get_logger(__name__)
 statements = load_yaml()
 q_bank_path = Path(os.getenv('DATA_DIR_QUESTIONS')).resolve()
+q_bank = None
+db_path = os.getenv('DATA_DIR')
+
+USR_PK = 1
+FUN_PK = 2
+
+
+def load_q_bank():
+    global q_bank
+    if q_bank is None:
+        with open(q_bank_path, 'r', encoding='utf-8') as file:
+            q_bank = json.load(file)
+    return q_bank
 
 def get_next_example_from_db(usr_pk: int, fun_pk: int) -> tuple[int, str, str, str]:
     """
@@ -21,39 +34,54 @@ def get_next_example_from_db(usr_pk: int, fun_pk: int) -> tuple[int, str, str, s
          (question_id, question_text, answer_text, passage_text)
     """
 
-    with open(q_bank_path, 'r', encoding='utf-8') as file:
-        jason_file = json.load(file)
-
+    jason_file = load_q_bank()
     question = sampling(statements=statements, j_file=jason_file, usr_id=usr_pk, fun_id=fun_pk)
     log_loc.info(f'{question['q_id']}, {question['question']}, {question['answer']}')
 
     with open(q_bank_path, 'w', encoding='utf-8') as file:
         json.dump(jason_file, file, ensure_ascii=False, indent=2)
 
-    question_id = question['q_id']
-    question_text = question['question']
-    answer_text = question['answer']
-    passage_text = question['context']
+    return question['q_id'], question['question'], question['answer'], question['context']
 
-    return question_id, question_text, answer_text, passage_text
+def get_example_by_id(q_id: int) -> tuple[int, str, str, str]:
+    """
 
-def save_annotation_to_db(question_id: int, flu: int, comp: int, fact: int,
-                          rej_q: bool, alt_q: str | None, rej_a: bool, alt_a: str | None) -> None:
+    :param q_id:
+    :return:
+    """
+
+    data = load_q_bank()
+    if not isinstance(data, list):
+        raise RuntimeError('Question bank must be a list of objects')
+
+    for q in data:
+        if q.get('q_id') == q_id:
+            return q["q_id"], q["question"], q["answer"], q["context"]
+    raise RuntimeError(f"No question found with q_id={q_id}")
+
+
+def save_annotation_to_db(qstn: str, q_id: int,  alt_q: str | None, passg: str, ansr: str, alt_a: str | None,
+                          flu: int, comp: int, fact: int, ann_id: int, q_acc: bool = True) -> None:
     """
     Takes Userinterface inputs which describe the answer to the question like how fluent, comprehensive and factual
     the answer is. It is called from the Flask app posting to the /submit_annotation.
 
     Args:
-        question_id (int): Unique ID to keep track of the answered and unanswered questions.
+        qstn (str): Question text in the Question Bank going to be rated.
+        q_id (int): Foreign-key of questions in the Question Bank.
+        alt_q (str | None): Question text provided by the annotator if they reject the original question.
+        passg (str): Passage text in the Question Bank.
+        ansr (str): Passage answer in the Question Bank.
+        alt_a (str | None): Answer text provided by the annotator if they reject the original answer.
         flu (int): Fluent parameter, describes how fluent the question is with ratings (1-5).
         comp (int): Comprehensive parameter, describes how comprehensive the question is with ratings (1-5).
         fact (int): Factual parameter, describes how factual the questions is with ratings (1-5).
-        rej_q (bool): Rejected question, set to True if the question is rejected.
-        alt_q (str | None): Alternative question, user input of a question example if original question is rejected.
-        rej_a (bool): Rejected answer, set to True if the answer is rejected.
-        alt_a (str | None): Alternative answer, user input of an answer example if original answer is rejected.
+        ann_id (int): Annotator Foreign-key of User table (current user).
+        q_acc (bool): Question accepted boolean default True.
+
     """
-    pass
+    a_data = [(qstn, q_id, alt_q, passg, ansr, alt_a, q_acc, flu, comp, fact, ann_id)]
+    db_push(data=a_data, db=db_path, table='annotations', statements=statements)
 
 def create_app() -> Flask:
     """
@@ -77,9 +105,8 @@ def create_app() -> Flask:
     def home():
         # Load one question
         flask_log.info('New Question loaded')
-
         try:
-            question_id, question_text, answer_text, passage_text = get_next_example_from_db(usr_pk=1, fun_pk=2)
+            question_id, question_text, answer_text, passage_text = get_next_example_from_db(usr_pk=USR_PK, fun_pk=FUN_PK)
         except RuntimeError as e:
             flask_log.info("No more questions for this user/function: %s", e)
             return render_template('index.html', no_questions=True)
@@ -90,32 +117,33 @@ def create_app() -> Flask:
 
     @app.post('/submit_annotation')
     def submit_annotation():
-        # Read values fro UI
+        # Read values from UI
         question_id = int(request.form['question_id'])
         fluency = int(request.form['fluency'])
         comprehensive = int(request.form['comprehensiveness'])
         factual = int(request.form['factuality'])
 
         # Optional alternatives from hidden fields
-        alternative_question = request.form.get('alternative_question', '').strip() or None
-        alternative_answer = request.form.get('alternative_answer', '').strip() or None
+        alt_quest = request.form.get('alternative_question', '').strip() or None
+        alt_ans = request.form.get('alternative_answer', '').strip() or None
 
-        # Flags: rejected, if alternative is given
-        rejected_q = alternative_question is not None
-        rejected_a = alternative_answer is not None
+        # Load original example from JSON via id
+        try:
+            q_id_db, question_text, answer_text, passage_text = get_example_by_id(question_id)
+        except RuntimeError as e:
+            flask_log.error("Could not load question %s from JSON: %s", question_id, e)
+            return "Question not found", 400
 
         # Store in DB
         flask_log.info(f'\nQuestion_id: {question_id}\nFluency: {fluency}\nComprehensiveness: {comprehensive}\nFactual: {factual}')
-        flask_log.info(f'Alternative Question: {alternative_question}')
-        flask_log.info(f'Alternative Answer: {alternative_answer}')
-        save_annotation_to_db(question_id=question_id, flu=fluency,
-                              comp=comprehensive, fact=factual,
-                              rej_q=rejected_q, alt_q=alternative_question,
-                              rej_a=rejected_a, alt_a=alternative_answer)
+        flask_log.info(f'Alternative Question: {alt_quest}')
+        flask_log.info(f'Alternative Answer: {alt_ans}')
+
+        save_annotation_to_db(qstn=question_text, q_id=question_id, alt_q=alt_quest, passg=passage_text, ansr=answer_text,
+                              alt_a=alt_ans, flu=fluency, comp=comprehensive, fact=factual, ann_id=USR_PK, q_acc=True)
 
         # Load next question
         return redirect(url_for('home'))
-
     return app
 
 def main() -> None:
