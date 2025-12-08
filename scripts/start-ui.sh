7f2179f57230c9230c4491ff797973c374311f73
sandro-roth <sandro.roth@hotmail.de>
2025-12-08 13:36:10 +0100

    simplify commands

--- scripts/start-ui.sh
+++ scripts/start-ui.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Check if db is up and running
-#/script/wait-for-db.sh
-
-# Start UI application
 echo 'Starting UI service ...'
-# src importable
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export PYTHONPATH="$SCRIPT_DIR/../src"
+
+# call wait for db here!
+
+# Start UI
 exec python -m sop_ui.app
