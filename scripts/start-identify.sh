43ffa983685271e3e13390c47e262f4738e55744
sandro-roth <sandro.roth@hotmail.de>
2025-12-08 13:40:36 +0100

    pretty up + start identify.sh

---
+++ scripts/start-identify.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+echo 'Starting identify service ...'
+
+exec python -m user_mask.main
--- scripts/start-ui.sh
+++ scripts/start-ui.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 echo 'Starting UI service ...'
 
 # call wait for db here!
 
-# Start UI
 exec python -m sop_ui.app
